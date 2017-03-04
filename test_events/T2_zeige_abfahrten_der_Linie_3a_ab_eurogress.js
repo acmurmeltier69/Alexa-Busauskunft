47fe2bc5b4095036f30b16ef90ba8e8f181d330d
Andreas_Kellner <Andreas_Kellner@ac-nb-kellner2.nuance.com>
2017-03-04 21:12:28 +0100

    new tests for invalid bus numbers

---
+++ test_events/T2_zeige_abfahrten_der_Linie_3a_ab_eurogress.js
@@ -0,0 +1,41 @@
+{
+  "session": {
+    "sessionId": "SessionId.f4572814-0ed9-4328-b679-8d22a7f2d3aa",
+    "application": {
+      "applicationId": "LOCAL_TEST"
+    },
+    "attributes": {},
+    "user": {
+      "userId": "USER_NEW"
+    },
+    "new": true
+  },
+  "request": {
+    "type": "IntentRequest",
+    "requestId": "EdwRequestId.e8c11008-11ef-4547-aea4-af77048c920d",
+    "locale": "de-DE",
+    "timestamp": "2017-02-22T22:51:31Z",
+    "intent": {
+      "name": "GetDeparturesFromOther",
+      "slots": {
+        "Origin": {
+          "name": "Origin",
+          "value": "eurogress"
+        },
+        "Next": {
+          "name": "Next"
+        },
+        "Transport": {
+          "name": "Transport"
+        },
+        "Direction": {
+          "name": "Direction"
+        },
+        "Busline": {
+          "name": "Busline"
+         }
+      }
+    }
+  },
+  "version": "1.0"
+}
